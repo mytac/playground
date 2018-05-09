f5714ee8a1f5fa2277063ddb22ad311ba5a76aab
张君卓 <my_tac@126.com>
2018-05-09 16:49:56 +0800

    添加参数验证

--- uploadImg/index.js
+++ uploadImg/index.js
@@ -1,10 +1,49 @@
-function renderTable(id){
-    const el=document.getElementById(id)
+function render(el) {
+    const element = $(el)
+    const imgChild=`<div class="img-wrapper" >
+            <div class="clear-img">x</div>
+            <img src="" width="100px" height="100px"/>
+        </div>`
+    return {
+        init() {
+            const template = `
+            <div class="img-group">
+                ${imgChild}
+             </div>`
+            const btnTemplate=`<input type="file" name="upload" style="display: block;"/>
+    <input type="button" name="add" style="display: block;" value="add"/>`
+            element.append(template,btnTemplate)
+        },
+        imgChild() {
+            // return
+        }
+    }
+}
 
-    const w=document.createElement('div')
-    el.appendChild(pic)
+function bindEvents() {
+
 
 }
 
-renderTable('myTable')
+function request() {
 
+}
+
+function pikiUploader(options) {
+    try{
+        if(!options){
+            throw 'no options'
+        }
+        const {el}=options
+        if(!el) throw 'no required params'
+        const renderBody=render(el)
+        renderBody.init()
+        bindEvents()
+        request()
+
+    }catch(e){
+        console.error(e)
+    }
+}
+
+window.pikiUploader = pikiUploader
