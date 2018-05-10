ca7d7f01536e3baad541aab20c2ead4abf435964
张君卓 <my_tac@126.com>
2018-05-10 11:07:02 +0800

    调通图片上传功能

--- uploadImg/index.js
+++ uploadImg/index.js
@@ -1,47 +1,94 @@
 function render(el) {
     const element = $(el)
-    const imgChild=`<div class="img-wrapper" >
-            <div class="clear-img">x</div>
-            <img src="" width="100px" height="100px"/>
-        </div>`
     return {
+        imgUrl(imgUrl) {
+            return `<div class="img-wrapper" >
+            <div class="clear-img">x</div>
+            <img src="${imgUrl}" width="100px" height="100px"/>
+        </div>`
+        },
         init() {
             const template = `
             <div class="img-group">
-                ${imgChild}
              </div>`
-            const btnTemplate=`<input type="file" name="upload" style="display: block;"/>
-    <input type="button" name="add" style="display: block;" value="add"/>`
-            element.append(template,btnTemplate)
+            const btnTemplate = `<input type="file" name="upload" id="pikiUpload"/>
+                                <input type="button" name="add" value="add" id="pikiAdd"/>`
+            element.append(template, btnTemplate)
         },
-        imgChild() {
-            // return
+        addImgChild(url) {
+            const imgTemplate = this.imgUrl(url)
+            $('.img-group').append(imgTemplate)
         }
     }
 }
 
-function bindEvents() {
+function bindEvents(el, postUrl) {
 
+    /*事件委托*/
+    function delegateEvent(tag, func, type = 'click') {
+        return $(el).on(type, tag, func)
+    }
+
+    delegateEvent('.clear-img', (e) => {
+        console.log(111)
+    })
+    delegateEvent('#pikiAdd', () => {
+        console.log('pikiAdd')
+    })
+
+    // 上传按钮
+    delegateEvent('#pikiUpload', (e) => {
+        const el = e.currentTarget
+        const file = el.files[0]
+        // 检查类型
+        if (file.type.indexOf('image') < 0) {
+            alert('请上传图片类型!!')
+            el.value = ''
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        request(postUrl, formData).then((data) => {
+                render().addImgChild(data)
+            },
+            (err) => {
+                console.log('err', err)
+            })
+
+
+    }, 'change')
 
 }
 
-function request() {
+
+function request(postUrl, fileUrl) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: postUrl,
+            crossDomain: true,
+            data: fileUrl,
+            dataType: 'json',
+            method: 'POST',
+            contentType: false,
+            processData: false,
+        }).done((data) => resolve(data.body)).fail((e) => reject(e));
+    })
 
 }
 
 function pikiUploader(options) {
-    try{
-        if(!options){
-            throw 'no options'
-        }
-        const {el}=options
-        if(!el) throw 'no required params'
-        const renderBody=render(el)
-        renderBody.init()
-        bindEvents()
-        request()
+    try {
+        if (!options) throw 'no options'
+        const {el, postUrl} = options
+        if (!el || !postUrl) throw 'no required params'
 
-    }catch(e){
+        const renderBody = render(el)
+        renderBody.init()
+        bindEvents(el, postUrl)
+
+    } catch (e) {
         console.error(e)
     }
 }
